e567e384f04f7f3ba27da6a3462cf1887e32da4f
oldgeezr <ramstadevensen@hotmail.com>
2015-02-03 19:50:23 +0100

    add interrupts incomplete

--- exercises/ex_1/ex1.s
+++ exercises/ex_1/ex1.s
@@ -91,33 +91,91 @@ _reset:
         orr r2, r2, r3
         str r2, [r1, #CMU_HFPERCLKEN0]
 
-        // Enable drive strenght
+        // Enable drive strength
         ldr r1, gpio_pa_base_addr
         ldr r2, [r1, #GPIO_CTRL]
-        mov r3, #2
+        ldr r3, =0x2
         orr r2, r2, r3
         str r2, [r1, #GPIO_CTRL]
 
-        // Set pin dir MODEH
-        ldr r1, gpio_pa_base_addr
+        // Set pin diretion in MODEH (8-15)
         ldr r2, [r1, #GPIO_MODEH]
         ldr r3, =0x55555555
-        // Temporaily setting only two leds weird shit
         orr r2, r2, r3
         str r2, [r1, #GPIO_MODEH]
 
-        ldr r1, gpio_pa_base_addr
-        ldr r2, [r1, #GPIO_DOUT]
-        ldr r3, =0x00000000
-        // Temporaily setting only two leds weird shit
+        // MOV instruction only support 8 bit. Why?
+
+        // Set LEDS
+        // mov r2, #0x55
+        // lsl r2, r2, #8
+        // str r2, [r1, #GPIO_DOUT]
+
+        // Enable input and pull direction by DOUT
+        ldr r1, gpio_pc_base_addr
+        ldr r2, [r1, #GPIO_MODEL]
+        ldr r3, =0x33333333
         orr r2, r2, r3
+        str r2, [r1, #GPIO_MODEL]
+
+        // Enable pull-up
+        ldr r2, =0xff
         str r2, [r1, #GPIO_DOUT]
+
+        // Enable interrupt on port C
+        ldr r1, gpio_base_addr
+        ldr r2, [r1, #GPIO_EXTIPSELL]
+        ldr r3, =0x22222222
+        orr r2, r2, r3
+        str r2, [r1, #GPIO_EXTIPSELL]
+
+        ldr r2, [r1, #GPIO_EXTIRISE]
+        ldr r3, =0xff
+        orr r2, r2, r3
+        str r2, [r1, #GPIO_EXTIRISE]
+
+        ldr r2, [r1, #GPIO_EXTIFALL]
+        ldr r3, =0xff
+        orr r2, r2, r3
+        str r2, [r1, #GPIO_EXTIFALL]
+
+        ldr r2, [r1, #GPIO_IEN]
+        ldr r3, =0xff
+        orr r2, r2, r3
+        str r2, [r1, #GPIO_IEN]
+
+        // Enable interrupt handling
+        ldr r1, nvic_iser0_addr
+        ldr r2, =0x802
+        str r2, [r1, #0]
+
+// Read btn state and update leds accordingly
+btn_poll:
+
+        //ldr r3, gpio_pa_base_addr
+        //ldr r2, [r1, #GPIO_DIN]
+        //lsl r2, r2, #8
+        //str r2, [r3, #GPIO_DOUT]
+        b btn_poll
 
 cmu_base_addr:
         .long CMU_BASE
 
+// Base address for LEDS
 gpio_pa_base_addr:
         .long GPIO_PA_BASE
+
+// Base address for GPIO
+gpio_base_addr:
+        .long GPIO_BASE
+
+// Base address for BTNS
+gpio_pc_base_addr:
+        .long GPIO_PC_BASE
+
+// Address for ISER0
+nvic_iser0_addr:
+        .long ISER0
 
 	/////////////////////////////////////////////////////////////////////////////
 	//
@@ -129,7 +187,12 @@ gpio_pa_base_addr:
         .thumb_func
 gpio_handler:
 
-	      b .  // do nothing
+        ldr r3, gpio_pa_base_addr
+        ldr r1, gpio_base_addr
+        ldr r2, [r1, #GPIO_IF]
+        lsl r2, r2, #8
+        str r2, [r3, #GPIO_DOUT]
+        str r2, [r1, #GPIO_IFC]
 
 	/////////////////////////////////////////////////////////////////////////////
 
