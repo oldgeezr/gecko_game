5334e3bdd2d454856eb64cb2a40832c3de0a26da
oldgeezr <ramstadevensen@hotmail.com>
2015-02-05 16:15:32 +0100

    clean indent

--- exercises/ex_1/ex1.s
+++ exercises/ex_1/ex1.s
@@ -1,85 +1,85 @@
         .syntax unified
 
-	      .include "efm32gg.s"
+        .include "efm32gg.s"
 
-	/////////////////////////////////////////////////////////////////////////////
-	//
+  /////////////////////////////////////////////////////////////////////////////
+  //
   // Exception vector table
   // This table contains addresses for all exception handlers
-	//
-	/////////////////////////////////////////////////////////////////////////////
+  //
+  /////////////////////////////////////////////////////////////////////////////
 
         .section .vectors
 
-	      .long   stack_top               /* Top of Stack                 */
-	      .long   _reset                  /* Reset Handler                */
-	      .long   dummy_handler           /* NMI Handler                  */
-	      .long   dummy_handler           /* Hard Fault Handler           */
-	      .long   dummy_handler           /* MPU Fault Handler            */
-	      .long   dummy_handler           /* Bus Fault Handler            */
-	      .long   dummy_handler           /* Usage Fault Handler          */
-	      .long   dummy_handler           /* Reserved                     */
-	      .long   dummy_handler           /* Reserved                     */
-	      .long   dummy_handler           /* Reserved                     */
-	      .long   dummy_handler           /* Reserved                     */
-	      .long   dummy_handler           /* SVCall Handler               */
-	      .long   dummy_handler           /* Debug Monitor Handler        */
-	      .long   dummy_handler           /* Reserved                     */
-	      .long   dummy_handler           /* PendSV Handler               */
-	      .long   dummy_handler           /* SysTick Handler              */
+        .long   stack_top               /* Top of Stack                 */
+        .long   _reset                  /* Reset Handler                */
+        .long   dummy_handler           /* NMI Handler                  */
+        .long   dummy_handler           /* Hard Fault Handler           */
+        .long   dummy_handler           /* MPU Fault Handler            */
+        .long   dummy_handler           /* Bus Fault Handler            */
+        .long   dummy_handler           /* Usage Fault Handler          */
+        .long   dummy_handler           /* Reserved                     */
+        .long   dummy_handler           /* Reserved                     */
+        .long   dummy_handler           /* Reserved                     */
+        .long   dummy_handler           /* Reserved                     */
+        .long   dummy_handler           /* SVCall Handler               */
+        .long   dummy_handler           /* Debug Monitor Handler        */
+        .long   dummy_handler           /* Reserved                     */
+        .long   dummy_handler           /* PendSV Handler               */
+        .long   dummy_handler           /* SysTick Handler              */
 
-	      /* External Interrupts */
-	      .long   dummy_handler
-	      .long   gpio_handler            /* GPIO even handler */
-	      .long   dummy_handler
-	      .long   dummy_handler
-	      .long   dummy_handler
-	      .long   dummy_handler
-	      .long   dummy_handler
-	      .long   dummy_handler
-	      .long   dummy_handler
-	      .long   dummy_handler
-	      .long   dummy_handler
-	      .long   gpio_handler            /* GPIO odd handler */
-	      .long   dummy_handler
-	      .long   dummy_handler
-	      .long   dummy_handler
-	      .long   dummy_handler
-	      .long   dummy_handler
-	      .long   dummy_handler
-	      .long   dummy_handler
-	      .long   dummy_handler
-	      .long   dummy_handler
-	      .long   dummy_handler
-	      .long   dummy_handler
-	      .long   dummy_handler
-	      .long   dummy_handler
-	      .long   dummy_handler
-	      .long   dummy_handler
-	      .long   dummy_handler
-	      .long   dummy_handler
-	      .long   dummy_handler
-	      .long   dummy_handler
-	      .long   dummy_handler
-	      .long   dummy_handler
-	      .long   dummy_handler
-	      .long   dummy_handler
-	      .long   dummy_handler
-	      .long   dummy_handler
-	      .long   dummy_handler
-	      .long   dummy_handler
+        /* External Interrupts */
+        .long   dummy_handler
+        .long   gpio_handler            /* GPIO even handler */
+        .long   dummy_handler
+        .long   dummy_handler
+        .long   dummy_handler
+        .long   dummy_handler
+        .long   dummy_handler
+        .long   dummy_handler
+        .long   dummy_handler
+        .long   dummy_handler
+        .long   dummy_handler
+        .long   gpio_handler            /* GPIO odd handler */
+        .long   dummy_handler
+        .long   dummy_handler
+        .long   dummy_handler
+        .long   dummy_handler
+        .long   dummy_handler
+        .long   dummy_handler
+        .long   dummy_handler
+        .long   dummy_handler
+        .long   dummy_handler
+        .long   dummy_handler
+        .long   dummy_handler
+        .long   dummy_handler
+        .long   dummy_handler
+        .long   dummy_handler
+        .long   dummy_handler
+        .long   dummy_handler
+        .long   dummy_handler
+        .long   dummy_handler
+        .long   dummy_handler
+        .long   dummy_handler
+        .long   dummy_handler
+        .long   dummy_handler
+        .long   dummy_handler
+        .long   dummy_handler
+        .long   dummy_handler
+        .long   dummy_handler
+        .long   dummy_handler
 
-	      .section .text
+        .section .text
 
-	/////////////////////////////////////////////////////////////////////////////
-	//
-	// Reset handler
+  /////////////////////////////////////////////////////////////////////////////
+  //
+  // Reset handler
   // The CPU will start executing here after a reset
-	//
-	/////////////////////////////////////////////////////////////////////////////
+  //
+  /////////////////////////////////////////////////////////////////////////////
 
-	      .globl  _reset
-	      .type   _reset, %function
+        .globl  _reset
+        .type   _reset, %function
 
     .thumb_func
 _reset:
@@ -141,59 +141,59 @@ _reset:
         ldr r2, =0x802
         str r2, [r1, #0]
 
-		//Enable deepsleep mode
-		ldr r1, =SCR
-		mov r2, #6
-		str r2, [r1]
+        //Enable deepsleep mode
+        ldr r1, =SCR
+        mov r2, #6
+        str r2, [r1]
 
-		//bl btn_polling
+        bl btn_polling
 
-		//Wait for interrupt
-		wfi
+        //Wait for interrupt
+        //wfi
 
-	/////////////////////////////////////////////////////////////////////////////
-	//
-	// Polling function
+  /////////////////////////////////////////////////////////////////////////////
+  //
+  // Polling function
   // Light LED based on BTN pressed
-	//
-	/////////////////////////////////////////////////////////////////////////////
+  //
+  /////////////////////////////////////////////////////////////////////////////
 
-	.thumb_func
+  .thumb_func
 btn_polling:
-		ldr r1, =GPIO_PC_BASE
-		ldr r2, =GPIO_PA_BASE
+        ldr r1, =GPIO_PC_BASE
+        ldr r2, =GPIO_PA_BASE
 while:
-		ldr r3, [r1, #GPIO_DIN]
-		lsl r3, r3, #8
-		str r3, [r2, #GPIO_DOUT]
-		b while
+        ldr r3, [r1, #GPIO_DIN]
+        lsl r3, r3, #8
+        str r3, [r2, #GPIO_DOUT]
+        b while
 
-	/////////////////////////////////////////////////////////////////////////////
-	//
+  /////////////////////////////////////////////////////////////////////////////
+  //
   // GPIO handler
   // The CPU will jump here when there is a GPIO interrupt
-	//
-	/////////////////////////////////////////////////////////////////////////////
+  //
+  /////////////////////////////////////////////////////////////////////////////
 
     .thumb_func
 gpio_handler:
 
-		//Clear ISR flag
-		ldr r1, =GPIO_BASE
+        //Clear ISR flag
+        ldr r1, =GPIO_BASE
         ldr r2, [r1, #GPIO_IF]
         str r2, [r1, #GPIO_IFC]
 
         //Light LED based on BTN pressed
         ldr r1, =GPIO_PC_BASE
-	    ldr r2, =GPIO_PA_BASE
+        ldr r2, =GPIO_PA_BASE
 
-		ldr r3, [r1, #GPIO_DIN]
-	    lsl r3, r3, #8
-	    str r3, [r2, #GPIO_DOUT]
+        ldr r3, [r1, #GPIO_DIN]
+        lsl r3, r3, #8
+        str r3, [r2, #GPIO_DOUT]
 
         bx lr
 
-	/////////////////////////////////////////////////////////////////////////////
+  /////////////////////////////////////////////////////////////////////////////
 
     .thumb_func
 dummy_handler:
