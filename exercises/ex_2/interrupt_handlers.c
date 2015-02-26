dbe5a4af8454dd9e85e1499e1515b31b92427812
Christoffer <ramstadevensen@hotmail.com>
2015-02-26 20:17:45 +0100

    minor in the interrupt handlers

--- exercises/ex_2/interrupt_handlers.c
+++ exercises/ex_2/interrupt_handlers.c
@@ -4,22 +4,26 @@
 #include "efm32gg.h"
 
 /* TIMER1 interrupt handler */
-void __attribute__ ((interrupt)) TIMER1_IRQHandler() 
-{  
+void __attribute__ ((interrupt)) TIMER1_IRQHandler()
+{
+  *TIMER1_IFC = 1;
+
+  // Put sample on DAC and divide by 2 (health insurance)
   /*
-    TODO feed new samples to the DAC
-    remember to clear the pending interrupt by writing 1 to TIMER1_IFC
-  */  
+  sample = sample/2;
+  *DAC0CH0DATA = sample;
+  *DAC0CH1DATA = sample;
+  */
 }
 
 /* GPIO even pin interrupt handler */
-void __attribute__ ((interrupt)) GPIO_EVEN_IRQHandler() 
+void __attribute__ ((interrupt)) GPIO_EVEN_IRQHandler()
 {
     /* TODO handle button pressed event, remember to clear pending interrupt */
 }
 
 /* GPIO odd pin interrupt handler */
-void __attribute__ ((interrupt)) GPIO_ODD_IRQHandler() 
+void __attribute__ ((interrupt)) GPIO_ODD_IRQHandler()
 {
     /* TODO handle button pressed event, remember to clear pending interrupt */
 }
