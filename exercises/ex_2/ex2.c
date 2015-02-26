5929e550d608463fdd8819d8bf2aea4d67dafe4b
Christoffer <ramstadevensen@hotmail.com>
2015-02-26 19:31:55 +0100

    merge timer into master

--- exercises/ex_2/ex2.c
+++ exercises/ex_2/ex2.c
@@ -3,102 +3,54 @@
 
 #include "efm32gg.h"
 
-/* 
-  TODO calculate the appropriate sample period for the sound wave(s) 
+/*
+  TODO calculate the appropriate sample period for the sound wave(s)
   you want to generate. The core clock (which the timer clock is derived
   from) runs at 14 MHz by default. Also remember that the timer counter
   registers are 16 bits.
 */
-/* The period between sound samples, in clock cycles */
-#define   SAMPLE_PERIOD   0
+
+#define SAMPLE_FREQ 44100
 
 /* Declaration of peripheral setup functions */
-void setupTimer(uint32_t period);
+void setupGPIO();
 void setupDAC();
-void setupNVIC();
+void setupTimer(uint32_t period);
+static inline void enterEMx(void);
+static inline void waitForInterrupt(void);
+static inline void setupNVIC(void);
 
 /* Your code will start executing here */
-int main(void) 
-{  
+int main(void)
+{
   /* Call the peripheral setup functions */
   setupGPIO();
   setupDAC();
-  setupTimer(SAMPLE_PERIOD);
-  
+  setupTimer(SAMPLE_FREQ);
+
   /* Enable interrupt handling */
   setupNVIC();
-  
-  /* TODO for higher energy efficiency, sleep while waiting for interrupts
-     instead of infinite loop for busy-waiting
-  */
-  while(1);
 
-  return 0;
+  // Change "x" with desired EM
+  enterEMx();
+
+  // Turn OFF clk and wait for interrupt
+  waitForInterrupt();
+
+  for (;;) {};
 }
 
-void setupNVIC()
+static inline void enterEMx(void)
 {
-  /* TODO use the NVIC ISERx registers to enable handling of interrupt(s)
-     remember two things are necessary for interrupt handling:
-      - the peripheral must generate an interrupt signal
-      - the NVIC must be configured to make the CPU handle the signal
-     You will need TIMER1, GPIO odd and GPIO even interrupt handling for this
-     assignment.
-  */
+  *SCR = 6;
+}
+static inline void waitForInterrupt(void)
+{
+  __asm volatile ("wfi");
 }
 
-/* if other interrupt handlers are needed, use the following names: 
-   NMI_Handler
-   HardFault_Handler
-   MemManage_Handler
-   BusFault_Handler
-   UsageFault_Handler
-   Reserved7_Handler
-   Reserved8_Handler
-   Reserved9_Handler
-   Reserved10_Handler
-   SVC_Handler
-   DebugMon_Handler
-   Reserved13_Handler
-   PendSV_Handler
-   SysTick_Handler
-   DMA_IRQHandler
-   GPIO_EVEN_IRQHandler
-   TIMER0_IRQHandler
-   USART0_RX_IRQHandler
-   USART0_TX_IRQHandler
-   USB_IRQHandler
-   ACMP0_IRQHandler
-   ADC0_IRQHandler
-   DAC0_IRQHandler
-   I2C0_IRQHandler
-   I2C1_IRQHandler
-   GPIO_ODD_IRQHandler
-   TIMER1_IRQHandler
-   TIMER2_IRQHandler
-   TIMER3_IRQHandler
-   USART1_RX_IRQHandler
-   USART1_TX_IRQHandler
-   LESENSE_IRQHandler
-   USART2_RX_IRQHandler
-   USART2_TX_IRQHandler
-   UART0_RX_IRQHandler
-   UART0_TX_IRQHandler
-   UART1_RX_IRQHandler
-   UART1_TX_IRQHandler
-   LEUART0_IRQHandler
-   LEUART1_IRQHandler
-   LETIMER0_IRQHandler
-   PCNT0_IRQHandler
-   PCNT1_IRQHandler
-   PCNT2_IRQHandler
-   RTC_IRQHandler
-   BURTC_IRQHandler
-   CMU_IRQHandler
-   VCMP_IRQHandler
-   LCD_IRQHandler
-   MSC_IRQHandler
-   AES_IRQHandler
-   EBI_IRQHandler
-   EMU_IRQHandler
-*/
+static inline void setupNVIC(void)
+{
+  // Enable interrupt for TIMER1 and GPIO_EVEN
+  *ISER0 |= 0x802;
+}
--- exercises/ex_2/timer.c
+++ exercises/ex_2/timer.c
@@ -3,19 +3,22 @@
 
 #include "efm32gg.h"
 
+#define FCPU 14000000UL
+
 /* function to setup the timer */
-void setupTimer(uint16_t period)
+void setupTimer(uint16_t freq)
 {
-  /*
-    TODO enable and set up the timer
-    
-    1. Enable clock to timer by setting bit 6 in CMU_HFPERCLKEN0
-    2. Write the period to register TIMER1_TOP
-    3. Enable timer interrupt generation by writing 1 to TIMER1_IEN
-    4. Start the timer by writing 1 to TIMER1_CMD
-    
-    This will cause a timer interrupt to be generated every (period) cycles. Remember to configure the NVIC as well, otherwise the interrupt handler will not be invoked.
-  */  
+  *CMU_HFPERCLKEN0 |= CMU2_HFPERCLKEN0_TIMER1;
+  *TIMER1_TOP = FCPU/freq;
+  *TIMER1_IEN = 1;
+  *TIMER1_CMD = 1;
 }
 
-
+/* function to disable the timer */
+void disableTimer(void)
+{
+  *CMU_HFPERCLKEN0 |= ~CMU2_HFPERCLKEN0_TIMER1;
+  *TIMER1_TOP = 0;
+  *TIMER1_IEN = 0;
+  *TIMER1_CMD = 0;
+}
