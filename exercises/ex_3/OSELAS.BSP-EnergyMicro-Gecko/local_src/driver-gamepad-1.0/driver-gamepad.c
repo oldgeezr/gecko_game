f670c74b651a06800a63a26e2e330be5317671d9
Christoffer <ramstadevensen@gmail.com>
2015-04-14 23:47:23 +0200

    make gamepad driver with noe errors, just two warnings

--- exercises/ex_3/OSELAS.BSP-EnergyMicro-Gecko/local_src/driver-gamepad-1.0/driver-gamepad.c
+++ exercises/ex_3/OSELAS.BSP-EnergyMicro-Gecko/local_src/driver-gamepad-1.0/driver-gamepad.c
@@ -10,7 +10,7 @@
 #include <linux/module.h>
 #include <linux/init.h>
 #include <linux/version.h>
-#include <linux/types.h>
+//#include <linux/types.h>
 #include <linux/interrupt.h>
 //#include <linux/kdev_t.h>
 #include <linux/fs.h>
@@ -64,7 +64,7 @@ static struct file_operations my_fops = {
 	.read = gamepad_read,
 	.write = gamepad_write,
 	.open = gamepad_open,
-	.release = gamegamepadd_release
+	.release = gamepad_release
 };
 
 /*
