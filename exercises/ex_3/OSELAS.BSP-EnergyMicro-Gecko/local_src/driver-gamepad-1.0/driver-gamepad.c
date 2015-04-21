9416175948b4d75adcc265d682ce02ad8fad81ef
Christoffer <ramstadevensen@gmail.com>
2015-04-21 11:59:09 +0200

    driver finished with rise and fall for interrupt

--- exercises/ex_3/OSELAS.BSP-EnergyMicro-Gecko/local_src/driver-gamepad-1.0/driver-gamepad.c
+++ exercises/ex_3/OSELAS.BSP-EnergyMicro-Gecko/local_src/driver-gamepad-1.0/driver-gamepad.c
@@ -23,6 +23,7 @@
 #define GPIO_ODD_IRQ                18
 
 typedef unsigned int uint32_t;
+typedef unsigned short uint16_t;
 typedef unsigned char uint8_t;
 
 static                dev_t devno;
@@ -31,6 +32,7 @@ static struct         class *cl;
 static struct         device* dev_create;
 struct fasync_struct* async_queue;
 void __iomem*         gpio_base;
+void __iomem*         gpio_pc_base;
 struct                siginfo signal;
 struct                task_struct *task;
 
@@ -62,7 +64,7 @@ int gamepad_release(struct inode *inode, struct file *filp) {
 ssize_t gamepad_read(struct file *filp, char __user *buff, size_t len, loff_t *offp) {
 
   int data;
-  data = ~ioread32(gpio_base + GPIO_PC_DIN);
+  data = ~ioread32(gpio_pc_base + GPIO_PC_DIN);
   copy_to_user(buff, &data, 1);
   printk(KERN_INFO "Gamepad driver: read()\n");
   return 0;
@@ -143,11 +145,11 @@ static int __init gampad_init(void) {
   request_mem_region(GPIO_PA_BASE, GPIO_IFC - GPIO_PA_BASE, NAME);
 
   // make memory base address for GPIO_PC
-  gpio_base = ioremap_nocache(GPIO_PC_BASE, GPIO_IFC - GPIO_PA_BASE);
+  gpio_pc_base = ioremap_nocache(GPIO_PC_BASE, GPIO_IFC - GPIO_PA_BASE);
 
   // set button pins to input
-  iowrite32(PIN_MODE_INPUT_PULL_FILTER, gpio_base + GPIO_PC_MODEL);
-  iowrite32(0xFF, gpio_base + GPIO_PC_DOUT);
+  iowrite32(PIN_MODE_INPUT_PULL_FILTER, gpio_pc_base + GPIO_PC_MODEL);
+  iowrite32(0xFF, gpio_pc_base + GPIO_PC_DOUT);
 
   // make memory base address for requested memory region
   gpio_base = ioremap_nocache(GPIO_PA_BASE, GPIO_IFC - GPIO_PA_BASE);
@@ -158,6 +160,7 @@ static int __init gampad_init(void) {
 
   // trigger interrupt on btn press
   iowrite32(0xFF, gpio_base + GPIO_EXTIFALL);
+  iowrite32(0xFF, gpio_base + GPIO_EXTIRISE);
 
   // clear interrupt flags
   iowrite32(0xFFFF, gpio_base + GPIO_IFC);
@@ -169,6 +172,7 @@ static int __init gampad_init(void) {
 static void __exit gamepad_cleanup(void) {
 
   iounmap(gpio_base);
+  iounmap(gpio_pc_base);
 
   device_destroy(cl, devno);
   class_destroy(cl);
@@ -183,7 +187,7 @@ static irqreturn_t interrupt_handler(int irq, void *dev_id) {
 
   iowrite32(0xFFFF, gpio_base + GPIO_IFC);
   kill_fasync(&async_queue, SIGIO, POLL_IN);
-  printk(KERN_INFO "Interrupt handled test 2...\n");
+  printk(KERN_INFO "Interrupt handled test...\n");
   return IRQ_HANDLED;
 }
 
